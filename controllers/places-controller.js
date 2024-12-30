f8cb0f23aa91c4ae1eb18eec4f002fb6d65ec325
EndritGashi20 <endrit.gash@student.uni-pr.edu>
2024-12-31 00:50:56 +0100

    creating controlers

---
+++ controllers/places-controller.js
@@ -0,0 +1,65 @@
+const HttpError = require('../models/http-error');
+
+const DUMMY_PLACES = [
+    {
+      id: 'p1',
+      title: 'Empire State Building',
+      description: 'One of the most famous sky scrapers in the world!',
+      location: {
+        lat: 40.7484474,
+        lng: -73.9871516
+      },
+      address: '20 W 34th St, New York, NY 10001',
+      creator: 'u1'
+    }
+  ];
+
+const getPlaceByUserId = (req, res, next) => {
+    const placeId = req.params.pid; // { pid: 'p1' }
+  
+    const place = DUMMY_PLACES.find(p => {
+      return p.id === placeId;
+    });
+  
+    if (!place) {
+      throw new HttpError('There is no place with this place id ', 404);
+    }
+  
+    res.json({ place }); // => { place } => { place: place }
+  };
+
+  const getUserById = (req, res, next) => {
+    const userId = req.params.uid;
+  
+    const place = DUMMY_PLACES.find(p => {
+      return p.creator === userId;
+    });
+  
+    if (!place) {
+      return next(
+        new HttpError('There is no place with this user id',404)
+      );
+    }
+  
+    res.json({ place });
+  }
+
+  const createPlace = (req, res, next) => {
+   const {title, description, cordinates, address, creator} = req.body;
+   //const title = req.body.title;
+    const createdPlace = {
+       id: ,
+        title,
+        description,
+        location: cordinates,
+        address,
+        creator
+    };
+
+    DUMMY_PLACES.push(createdPlace); // unshift(createdPlace)
+   res.status(201).json({place: createdPlace});
+  };
+
+  exports.getUserById = getUserById;
+  exports.getPlaceByUserId = getPlaceByUserId;
+  exports.createPlace = createPlace;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,50 +1,14 @@
-const exppress = require('express');
+const express = require('express');
 
-const router = exppress.Router();
+const placesControllers = require('../controllers/places-controller');
 
-const DUMMY_PLACES = [
-    {
-      id: 'p1',
-      title: 'Empire State Building',
-      description: 'One of the most famous sky scrapers in the world!',
-      imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-      address: '20 W 34th St, New York, NY 10001',
-      location: {
-        lat: 40.7484405,
-        lng: -73.9878584
-      },
-      creator: 'u1'
-    },
-    {
-      id: 'p2',
-      title: 'Empire State Building',
-      description: 'One of the most famous sky scrapers in the world!',
-      imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-      address: '20 W 34th St, New York, NY 10001',
-      location: {
-        lat: 40.7484405,
-        lng: -73.9878584
-      },
-      creator: 'u2'
-    }
-  ];
 
-router.get('/:pid',(req,res,next)=>{
-    const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find(p=>{
-        return p.id === placeId;
-    });
-    res.json({place: place});
-    });
+const router = express.Router();
 
-    router.get('/user/:uid',(req,res,netx)=>{
-    const userId=req.params.uid;
-    const user = DUMMY_PLACES.find(p=>{
-     return p.creator === userId;
-    });
-    res.json({user: user});
-    })
+router.get('/:pid',placesControllers.getUserById);
 
-    module.exports = router;
+router.get('/user/:uid',placesControllers.getPlaceByUserId);
+
+router.post('/', placesControllers.createPlace);
+
+module.exports = router;
